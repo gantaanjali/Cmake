e32579bd49b6210d1f9432702f113241ec76ec09
gantaanjali <52240494+gantaanjali@users.noreply.github.com>
2023-12-06 11:16:11 +0530

    Create header.h

---
+++ shared/header.h
@@ -0,0 +1,8 @@
+extern void hello(void);
+extern void add(int a,int b);
+extern void multi(int a,int b);
+extern int swap(int a,int b);
+extern int fact(int a);
+extern void sub(int a,int b);
+extern void bye(void);
+extern void closemain(void);
