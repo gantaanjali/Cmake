1c0a6f8bdd293f6df9f7b2d6b9f078f1a3f561d7
gantaanjali <52240494+gantaanjali@users.noreply.github.com>
2023-12-06 11:08:30 +0530

    Create file12.c

---
+++ static/file12.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"header.h"
+#include <unistd.h>
+void mylib2()
+{
+printf("\nI am in file2\n");
+
+}
+void callfun(void)
+{
+    printf("\n i am in callfun function\n");
+}
